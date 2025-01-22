6c219a52713c7d3ac7ee38291e714c7db354e529
Мерт Алиев <icsteamdal@gmail.com>
2025-01-22 10:50:05 +0200

    първа редакция

---
+++ Web/Data/AppDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Web.Models;
+
+namespace Web.Data
+{
+    public class AppDBContext :DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Genre>().HasData
+                (
+                new Genre { Id = 1, Name = "Фантастика" },
+                new Genre { Id = 2, Name = "Роман" },
+                new Genre { Id = 3, Name = "Повест" },
+                new Genre { Id = 4, Name = "Разказ" }
+                );
+        }
+    }
+}
